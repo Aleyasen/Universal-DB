c20070bbde480fdde7b6ed8b3aa6061048fd25df
Amirhossein Aleyasen <aleyase2@illinois.edu>
2015-08-14 11:52:27 -0500

    add configuration files for datasets and resolve other visualization issues

---
+++ utils.php
@@ -0,0 +1,12 @@
+<!-- Tab panes -->
+<div class="tab-content">
+    <div role="tabpanel" class="tab-pane active" id="network">
+        <?php include 'transformation.php'; ?>
+    </div>
+    <div role="tabpanel" class="tab-pane" id="ranking">
+        <?php include 'ranking.php'; ?>
+    </div>
+    <div role="tabpanel" class="tab-pane" id="algorithm">
+        <?php include 'network.php'; ?>
+    </div>
+</div>
